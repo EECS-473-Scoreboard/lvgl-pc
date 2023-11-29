f84f441c05ef377e881cb762a66ec2a01d5156db
Ruiqi Niu <rniu@umich.edu>
2023-11-29 16:49:24 -0500

    Game screen from Zach.

--- ui/simulator/src/game_screen.c
+++ ui/simulator/src/game_screen.c
@@ -1,15 +1,90 @@
-#include "common.h"
 #include "game_screen.h"
+#include "common.h"
 
+static lv_obj_t *ScoreScreenBtn;
+static lv_obj_t *RetBtn;
+static lv_obj_t *BTN_1;
+static lv_obj_t *BTN_2;
+static lv_obj_t *BTN_3;
+static lv_obj_t *BTN_4;
+static lv_obj_t *BTN_5;
 
-static lv_obj_t* P1Text;
-static lv_obj_t* P2Text;
-static lv_obj_t* P1Score;
-static lv_obj_t* ScoreScreenBtn;
+inline static void set_ret_btn_style(lv_obj_t *btn) {
+    lv_obj_set_width(btn, 150);
+    lv_obj_set_height(btn, 50);
+    lv_obj_add_flag(btn, LV_OBJ_FLAG_CHECKABLE);
 
+    lv_obj_t *retLabel = lv_label_create(btn);
+    lv_obj_center(retLabel);
+    lv_label_set_text(retLabel, "Return");
+}
 
+inline static void set_act_btn_style(lv_obj_t *btn, char name[10]) {
+    lv_obj_set_width(btn, 150);
+    lv_obj_set_height(btn, 50);
+    lv_obj_add_flag(btn, LV_OBJ_FLAG_CHECKABLE);
 
+    lv_obj_t *retLabel = lv_label_create(btn);
+    lv_obj_center(retLabel);
+    lv_label_set_text(retLabel, name);
+}
 
-void game_screen_build(lv_obj_t* scr){
+inline static void set_scor_btn_style(lv_obj_t *btn) {
+    lv_obj_set_width(btn, 150);
+    lv_obj_set_height(btn, 50);
+    lv_obj_add_flag(btn, LV_OBJ_FLAG_CHECKABLE);
 
+    lv_obj_t *retLabel = lv_label_create(btn);
+    lv_obj_center(retLabel);
+    lv_label_set_text(retLabel, "Scores");
+}
+
+void game_screen_build(lv_obj_t *scr) {
+    lv_obj_set_style_bg_color(scr, COLOR_GREY, LV_PART_MAIN);
+    lv_obj_t *title = lv_label_create(scr);
+    lv_label_set_text(title, "Current Game"); // Need to find a way to get current game as char[]
+    lv_obj_set_style_text_align(title, LV_TEXT_ALIGN_CENTER, 0);
+    lv_obj_align(title, LV_ALIGN_CENTER, 0, -360);
+
+    lv_obj_t *P1_name = lv_label_create(scr);
+    lv_label_set_text(P1_name, "Player 1"); // Need to get the player's name
+    lv_obj_align(P1_name, LV_ALIGN_CENTER, 0, -300);
+    lv_obj_t *p1Score = lv_table_create(scr);
+    lv_table_set_cell_value(p1Score, 0, 0, "Score1");
+    lv_obj_align(p1Score, LV_ALIGN_CENTER, 0, -260);
+
+    lv_obj_t *P2_name = lv_label_create(scr);
+    lv_label_set_text(P2_name, "Player 2"); // Need to get the player's name
+    lv_obj_align(P2_name, LV_ALIGN_CENTER, 0, -200);
+    lv_obj_t *p2Score = lv_table_create(scr);
+    lv_table_set_cell_value(p2Score, 0, 0, "Score2");
+    lv_obj_align(p2Score, LV_ALIGN_CENTER, 0, -160);
+
+    RetBtn = lv_btn_create(scr);
+    lv_obj_align(RetBtn, LV_ALIGN_CENTER, -80, 40);
+    set_ret_btn_style(RetBtn);
+
+    ScoreScreenBtn = lv_btn_create(scr);
+    lv_obj_align(ScoreScreenBtn, LV_ALIGN_CENTER, 80, 40);
+    set_scor_btn_style(ScoreScreenBtn);
+
+    BTN_1 = lv_btn_create(scr);
+    lv_obj_align(BTN_1, LV_ALIGN_CENTER, -80, 100);
+    set_act_btn_style(BTN_1, "P1+");
+
+    BTN_2 = lv_btn_create(scr);
+    lv_obj_align(BTN_2, LV_ALIGN_CENTER, -80, 160);
+    set_act_btn_style(BTN_2, "P1-");
+
+    BTN_3 = lv_btn_create(scr);
+    lv_obj_align(BTN_3, LV_ALIGN_CENTER, 80, 100);
+    set_act_btn_style(BTN_3, "P2+");
+
+    BTN_4 = lv_btn_create(scr);
+    lv_obj_align(BTN_4, LV_ALIGN_CENTER, 80, 160);
+    set_act_btn_style(BTN_4, "P2-");
+
+    BTN_5 = lv_btn_create(scr);
+    lv_obj_align(BTN_5, LV_ALIGN_CENTER, 0, 220);
+    set_act_btn_style(BTN_5, "Reset");
 }
